c38502f2cf7b9ebec3887296dcfcef0b138b050c
tt <tamara.trefilova@gmail.com>
2019-10-16 20:14:17 -0700

    created UserDetailsRequestModel class

---
+++ mobile-app-ws/src/main/java/com/apps/app/ws/app/ws/ui/model/request/UserDetailsRequestModel.java
@@ -0,0 +1,37 @@
+package com.apps.app.ws.app.ws.ui.model.request;
+
+public class UserDetailsRequestModel {
+	
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPasssword() {
+		return passsword;
+	}
+	public void setPasssword(String passsword) {
+		this.passsword = passsword;
+	}
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String passsword;
+	
+	
+
+}
